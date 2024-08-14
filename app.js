a6b70b4afa8c1ab1ee6c4d3f586f087c603c9405
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2024-08-14 17:44:22 +0400

    Init Discord Bot

--- app.js
+++ app.js
@@ -1,59 +1,165 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 import {
   InteractionType,
   InteractionResponseType,
+  InteractionResponseFlags,
+  MessageComponentTypes,
+  ButtonStyleTypes,
   verifyKeyMiddleware,
-} from 'discord-interactions';
-import { getRandomEmoji } from './utils.js';
+} from "discord-interactions";
+import { getRandomEmoji, DiscordRequest } from "./utils.js";
+import { getShuffledOptions, getResult } from "./game.js";
 
 // Create an express app
 const app = express();
 // Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
 
+// Store for in-progress games. In production, you'd want to use a DB
+const activeGames = {};
+
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  * Parse request body and verifies incoming requests using discord-interactions package
  */
-app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
-  // Interaction type and data
-  const { type, data } = req.body;
+app.post(
+  "/interactions",
+  verifyKeyMiddleware(process.env.PUBLIC_KEY),
+  async function (req, res) {
+    // Interaction type and data
+    const { type, id, data } = req.body;
 
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
-
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
-
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: `hello world ${getRandomEmoji()}`,
-        },
-      });
+    /**
+     * Handle verification requests
+     */
+    if (type === InteractionType.PING) {
+      return res.send({ type: InteractionResponseType.PONG });
     }
 
-    console.error(`unknown command: ${name}`);
-    return res.status(400).json({ error: 'unknown command' });
-  }
+    /**
+     * Handle slash command requests
+     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+     */
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      const { name } = data;
 
-  console.error('unknown interaction type', type);
-  return res.status(400).json({ error: 'unknown interaction type' });
-});
+      // "test" command
+      if (name === "test") {
+        // Send a message into the channel where command was triggered from
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            // Fetches a random emoji to send from a helper function
+            content: `It's going to be alright, Relurcigamgnik  ${getRandomEmoji()}`, //https://i.kym-cdn.com/photos/images/newsfeed/002/477/529/b46.gif
+          },
+        });
+      }
+
+      // "wiggle" command
+      if (name === "wiggle") {
+        // Send a message into the channel where command was triggered from
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            // Fetches a random emoji to send from a helper function
+            content: `https://cdn.discordapp.com/attachments/1181435156473585664/1272920199724798026/BocchiWiggle.gif`, //https://i.kym-cdn.com/photos/images/newsfeed/002/477/529/b46.gif
+          },
+        });
+      }
+      console.error(`unknown command: ${name}`);
+      return res.status(400).json({ error: "unknown command" });
+    }
+
+    /**
+     * Handle requests from interactive components
+     * See https://discord.com/developers/docs/interactions/message-components#responding-to-a-component-interaction
+     */
+    if (type === InteractionType.MESSAGE_COMPONENT) {
+      // custom_id set in payload when sending message component
+      const componentId = data.custom_id;
+
+      if (componentId.startsWith("accept_button_")) {
+        // get the associated game ID
+        const gameId = componentId.replace("accept_button_", "");
+        // Delete message with token in request body
+        const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+        try {
+          await res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: "What is your object of choice?",
+              // Indicates it'll be an ephemeral message
+              flags: InteractionResponseFlags.EPHEMERAL,
+              components: [
+                {
+                  type: MessageComponentTypes.ACTION_ROW,
+                  components: [
+                    {
+                      type: MessageComponentTypes.STRING_SELECT,
+                      // Append game ID
+                      custom_id: `select_choice_${gameId}`,
+                      options: getShuffledOptions(),
+                    },
+                  ],
+                },
+              ],
+            },
+          });
+          // Delete previous message
+          await DiscordRequest(endpoint, { method: "DELETE" });
+        } catch (err) {
+          console.error("Error sending message:", err);
+        }
+      } else if (componentId.startsWith("select_choice_")) {
+        // get the associated game ID
+        const gameId = componentId.replace("select_choice_", "");
+
+        if (activeGames[gameId]) {
+          // Interaction context
+          const context = req.body.context;
+          // Get user ID and object choice for responding user
+          // User ID is in user field for (G)DMs, and member for servers
+          const userId =
+            context === 0 ? req.body.member.user.id : req.body.user.id;
+          const objectName = data.values[0];
+          // Calculate result from helper function
+          const resultStr = getResult(activeGames[gameId], {
+            id: userId,
+            objectName,
+          });
+
+          // Remove game from storage
+          delete activeGames[gameId];
+          // Update message with token in request body
+          const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+
+          try {
+            // Send results
+            await res.send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: { content: resultStr },
+            });
+            // Update ephemeral message
+            await DiscordRequest(endpoint, {
+              method: "PATCH",
+              body: {
+                content: "Nice choice " + getRandomEmoji(),
+                components: [],
+              },
+            });
+          } catch (err) {
+            console.error("Error sending message:", err);
+          }
+        }
+      }
+    }
+
+    console.error("unknown interaction type", type);
+    return res.status(400).json({ error: "unknown interaction type" });
+  }
+);
 
 app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+  console.log("Listening on port", PORT);
 });
